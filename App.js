0a54849d8fb5a29669cf4b59f429240b70b99d2d
Rodrigo <rodrigojatib@gmail.com>
2023-07-13 12:32:13 -0300

    Se agrega el proyecto

---
+++ App.js
@@ -0,0 +1,16 @@
+import EnvioDeMail from "./Mails/eMail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EnvioDeMail/>}></Route>
+        </Routes>
+      </BrowserRouter> */}
+    </>
+  );
+}
+
+export default App;
